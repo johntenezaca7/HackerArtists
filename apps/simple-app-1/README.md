e156a68665bf58e1eb30f84968a9c187c7c89a5b
John Tenezaca <thenews0101001@gmail.com>
2019-11-16 13:47:16 -0500

    lesson one added

--- apps/simple-app-1/README.md
+++ apps/simple-app-1/README.md
@@ -1,1 +1,11 @@
-#Help
+``` 
+cd  apps/simple-app-1 
+npm install
+npm run dev
+```
+
+# Lesson One
+
+### Working with Data using the Star Wars API
+
+"https://swapi.co/api/"
--- apps/simple-app-1/public/js/main.js
+++ apps/simple-app-1/public/js/main.js
@@ -1,1 +1,5 @@
-console.log("I work!")
+var url = "https://swapi.co/api/films/1/";
+
+// Using fetch
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
